adc5091443d9a23154a5430d4fa1e754e4636d44
sookim-1 <sookim-1@users.noreply.github.com>
2025-08-19 09:03:12 +0000

    deploy: 9499642695a0ce97183f0d902c572cc67a59590f

--- Sources/k-swift-publish-blog/Extensions/String+URL.swift
+++
@@ -1,9 +0,0 @@
-import Foundation
-
-extension String {
-    func urlEncoded() -> String {
-        let allowedChars = CharacterSet.alphanumerics.union(.init(charactersIn: "/?-._~"))
-        let decoded = removingPercentEncoding ?? self
-        return decoded.addingPercentEncoding(withAllowedCharacters: allowedChars) ?? decoded
-    }
-}
